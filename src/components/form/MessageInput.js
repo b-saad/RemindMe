b6a3b795c68d8be203152c95650716dab62b95e4
Bilal Saad <bilalsaad923@gmail.com>
2019-12-26 18:27:04 -0500

    add live message validation, remove dark theme

--- src/components/form/MessageInput.js
+++ src/components/form/MessageInput.js
@@ -7,18 +7,41 @@ class MessageInput extends Component {
     constructor () {
         super();
         this.state = {
+            message: '',
             characterCount: CHARACTER_LIMIT,
-            errorMessage: NO_MESSAGE_ERROR
-        }
+            errorMessage: NO_MESSAGE_ERROR,
+            error: false
+        };
+        this.handleMessageChange = this.handleMessageChange.bind(this);
     }
+
+    handleMessageChange(e) {
+        const length = e.target.value.length;
+        let errorMessage = '';
+        let error = false;
+        if (length == 0) {
+            errorMessage = NO_MESSAGE_ERROR;
+            error = true;
+        } else if (length > CHARACTER_LIMIT) {
+            errorMessage = EXCEED_CHARACTER_LIMIT;
+            error = true;
+        }
+        this.setState({
+            message : e.target.value,
+            characterCount: CHARACTER_LIMIT - e.target.value.length,
+            errorMessage: errorMessage,
+            error: error
+         });
+    }
+
     render() {
         return (
-            <div className={this.constructor.name}>
+            <div className={this.constructor.name }>
                 <p className='fieldLabel'>Enter your reminder below</p>
                 <p className='characterCount'>{this.state.characterCount}</p>
-                <textarea>
+                <textarea className={this.state.error ? 'inputError' : ''} value={this.state.message} onChange={this.handleMessageChange}>
                 </textarea>
-                <p className='errorLabel'>{this.state.errorMessage}</p>
+                <p className='errorLabel' hidden={!this.state.error}>{this.state.errorMessage}</p>
             </div>
         )
     }
--- src/components/form/ReminderForm.js
+++ src/components/form/ReminderForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import FormStyle from '../../css/ReminderForm.css'
+import '../../css/ReminderForm.css';
 
 import DateInput from './DateInput';
 import TimeInput from './TimeInput';
@@ -9,8 +9,7 @@ import SubmitButton from './SubmitButton'
 class ReminderForm extends Component {
     render() {
         return (
-            <form className={this.constructor.name} style={FormStyle}>
-                ReminderForm
+            <form className={this.constructor.name}>
                 <DateInput />
                 <TimeInput />
                 <MessageInput />
