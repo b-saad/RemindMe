cd0b4af6f82cdf01d966aea26924331f7257ebcf
Bilal Saad <bilalsaad923@gmail.com>
2019-12-30 00:26:51 -0500

    lift message error to form component and add validation

--- src/components/form/MessageInput.js
+++ src/components/form/MessageInput.js
@@ -1,46 +1,30 @@
 import React, { Component } from 'react';
 
-const CHARACTER_LIMIT = 160;
-const NO_MESSAGE_ERROR = 'Please enter a message to be sent';
-const EXCEED_CHARACTER_LIMIT = `Your message must be under ${CHARACTER_LIMIT} characters`
 class MessageInput extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            characterCount: CHARACTER_LIMIT,
-            errorMessage: NO_MESSAGE_ERROR,
-            error: false
+            characterCount: this.props.charLimit
         };
         this.handleMessageChange = this.handleMessageChange.bind(this);
     }
 
     handleMessageChange(e) {
-        const length = e.target.value.length;
-        let errorMessage = '';
-        let error = false;
-        if (length === 0) {
-            errorMessage = NO_MESSAGE_ERROR;
-            error = true;
-        } else if (length > CHARACTER_LIMIT) {
-            errorMessage = EXCEED_CHARACTER_LIMIT;
-            error = true;
-        }
         this.setState({
-            characterCount: CHARACTER_LIMIT - e.target.value.length,
-            errorMessage: errorMessage,
-            error: error
+            characterCount: this.props.charLimit - e.target.value.length
         });
         this.props.handleMessageChange(e);
     }
 
     render() {
+        const { message, error, errorMessage } = this.props;
         return (
             <div className={this.constructor.name }>
                 <p className='fieldLabel'>Enter your reminder below</p>
                 <p className='characterCount'>{`(${this.state.characterCount})`}</p>
-                <textarea className={this.state.error ? 'inputError' : ''} value={this.props.message} onChange={this.handleMessageChange}>
+                <textarea className={error ? 'inputError' : ''} value={message} onChange={this.handleMessageChange}>
                 </textarea>
-                <p className='errorLabel' hidden={!this.state.error}>{this.state.errorMessage}</p>
+                <p className='errorLabel' hidden={!error}>{errorMessage}</p>
             </div>
         )
     }
--- src/components/form/ReminderForm.js
+++ src/components/form/ReminderForm.js
@@ -9,7 +9,9 @@ import TimeInput from './TimeInput';
 import MessageInput from './MessageInput'
 import SubmitButton from './SubmitButton'
 
-// const CHARACTER_LIMIT = 160;
+const CHARACTER_LIMIT = 160;
+const NO_MESSAGE_ERROR = 'Please enter a message to be sent';
+const EXCEED_CHARACTER_LIMIT = `Your message must be under ${CHARACTER_LIMIT} characters`
 class ReminderForm extends Component {
     constructor () {
         super();
@@ -18,21 +20,44 @@ class ReminderForm extends Component {
             date: formatDate(new Date()),
             time: getCurrentTime(),
             message: '',
+            messageCharLimit: CHARACTER_LIMIT,
             dateError: false,
-            timeError: false
+            timeError: false,
+            messageError: false,
+            messageErrorMessage: ''
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleDateChange = this.handleDateChange.bind(this);
         this.handleTimeChange = this.handleTimeChange.bind(this);
+        this.handleMessageChange = this.handleMessageChange.bind(this);
     }
 
     handleSubmit() {
-        console.log(getCurrentTime());
+        // TODO: update phone validation;
+        const phoneError = this.state.phone === '';
+        const dateError = !validDate(this.state.date);
+        const timeError = !dateError && isToday(this.state.date) && !validFutureTime(this.state.time);
+        const messageError = this.state.message === '' || this.state.messageError;
+        if (phoneError || dateError || timeError || messageError) {
+            this.setState({
+                dateError: dateError,
+                timeError: timeError
+            });
+            return;
+        }
+        const date = new Date(`${this.state.date}T${this.state.time}:00`).toISOString();
+        const phone = this.state.phone.replace(/[()]/g, '').replace(/\s/g, '').replace(/[-]/g, '');
+        const reqBody = {
+            "phoneNumber" : phone,
+            "message": this.state.message,
+            "date": date
+        };
+        console.log(reqBody);
     }
 
     handleDateChange(e) {
         const dateError = !validDate(e.target.value);
-        const timeError = validDate(e.target.value) && isToday(e.target.value) && !validFutureTime(this.state.time);
+        const timeError = !dateError && isToday(e.target.value) && !validFutureTime(this.state.time);
         this.setState({
             date: e.target.value,
             dateError: dateError,
@@ -51,14 +76,38 @@ class ReminderForm extends Component {
         })
     }
 
+    handleMessageChange(e) {
+        const length = e.target.value.length;
+        let errorMessage = '';
+        let error = false;
+        if (length === 0) {
+            errorMessage = NO_MESSAGE_ERROR;
+            error = true;
+        } else if (length > CHARACTER_LIMIT) {
+            errorMessage = EXCEED_CHARACTER_LIMIT;
+            error = true;
+        }
+        this.setState({
+            message: e.target.value,
+            messageError: error,
+            messageErrorMessage: errorMessage
+        })
+    }
+
     render() {
-        const { phone, date, dateError, time, timeError, message } = this.state;
+        const { phone, date, dateError, time, timeError, message, messageCharLimit, messageError, messageErrorMessage } = this.state;
         return (
             <form className={this.constructor.name}>
                 <PhoneInputField phone={phone} handlePhoneChange={phone => this.setState({phone: phone})}/>
                 <DateInput date={date} error={dateError} handleDateChange={this.handleDateChange} />
                 <TimeInput time={time} error={timeError} handleTimeChange={this.handleTimeChange}/>
-                <MessageInput message={message} handleMessageChange={e => this.setState({message: e.target.value})}/>
+                <MessageInput 
+                    message={message} 
+                    charLimit={messageCharLimit}
+                    error={messageError} 
+                    errorMessage={messageErrorMessage} 
+                    handleMessageChange={this.handleMessageChange}
+                />
                 <SubmitButton handleClick={this.handleSubmit}/>
             </form>
         )
